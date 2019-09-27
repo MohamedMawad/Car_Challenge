990947a653412e338ea60d31f4623dee0332206c
MohamedMawad <mohamedmoawad1995@gmail.com>
2019-09-27 23:57:34 +0200

    MICRO_setting file added

---
+++ LIB/MICRO_setting.h
@@ -0,0 +1,28 @@
+/*
+ * MICRO_SETTING.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: Mohamed ELameer
+ */
+
+#ifndef MICRO_SETTING_H_
+#define MICRO_SETTING_H_
+
+
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+
+// #include <avr/io.h>
+// //#include <avr/iom32.h>
+// //#include"avr/iom32.h"
+// #include <util/delay.h>
+// #include <avr/interrupt.h>
+#include "AVR_REG_LIB.h"
+#include "interrupt.h"
+#include "data_types.h"
+
+
+
+
+#endif /* MICRO_SETTING_H_ */
